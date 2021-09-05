dbad2452686fefdf09f3f7a25ad4dae4ac25db13
yangjie500 <kristeh97@gmail.com>
2021-09-06 00:01:31 +0800

    Testing

---
+++ logic.js
@@ -0,0 +1,39 @@
+const detailsBtn = document.querySelector(".btn-details");
+const addBtn = document.querySelector(".btn-add");
+const cancelBtn = document.querySelectorAll(".btn-cancel");
+
+
+function toggleModal(keyword) {
+    const modal = document.querySelector(`.modal-${keyword}`);
+    modal.style.display = 'block';
+}
+
+function closeModal() {
+    const modal = document.querySelectorAll(`.modal`);
+    modal.forEach(elem => {
+        elem.style.display = 'none';
+    })   
+}
+
+
+function logic() {
+    detailsBtn.addEventListener('click', (e) => {
+        const keyword = detailsBtn.textContent;
+        closeModal()
+        toggleModal(keyword);
+    });
+
+    addBtn.addEventListener('click', (e) => {
+        const keyword = addBtn.textContent;
+        closeModal()
+        toggleModal(keyword);
+    });
+
+    cancelBtn.forEach(elem => {
+        elem.addEventListener('click', (e) => {
+            closeModal();
+        }) 
+    });
+}
+
+window.onload = logic();
